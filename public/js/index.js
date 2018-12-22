4b522048d6a0e127be983e49387942732fd98a11
Pittawat2542 <pittawat2542@hotmail.com>
2018-12-22 16:26:59 +0700

    :sparkles: scroll to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,24 @@
 const socket = io();
 
+function scrollToBottom() {
+  // Selectors
+  let messages = $("#messages");
+  let newMessage = messages.children("li:last-child");
+  // Heights
+  let clientHeight = messages.prop("clientHeight");
+  let scrollTop = messages.prop("scrollTop");
+  let scrollHeight = messages.prop("scrollHeight");
+  let newMessageHeight = newMessage.innerHeight();
+  let lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (
+    clientHeight + scrollTop + newMessageHeight + lastMessageHeight >=
+    scrollHeight
+  ) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on("connect", function() {
   console.log("Connected to server");
 });
@@ -18,6 +37,7 @@ socket.on("newMessage", function(message) {
   });
 
   $("#messages").append(html);
+  scrollToBottom();
 });
 
 socket.on("newLocationMessage", function(message) {
@@ -30,6 +50,7 @@ socket.on("newLocationMessage", function(message) {
   });
 
   $("#messages").append(html);
+  scrollToBottom();
 });
 
 $("#message-form").on("submit", function(e) {
