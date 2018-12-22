01de7e7ce44b618839ab9b0dc557f9435783106d
Pittawat2542 <pittawat2542@hotmail.com>
2018-12-22 15:41:05 +0700

    :art: add style for chat app

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ io.on("connection", socket => {
 
   socket.on("createMessage", (message, callback) => {
     io.emit("newMessage", generateMessage(message.from, message.text));
-    callback("WWWWW");
+    callback();
   });
 
   socket.on("createLocationMessage", coords => {
